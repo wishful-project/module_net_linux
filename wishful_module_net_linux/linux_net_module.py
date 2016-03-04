94f0a94c310392f0423900f0e228f8da64c74f77
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-03-04 17:54:27 +0100

    init

---
+++ wishful_module_net_linux/linux_net_module.py
@@ -0,0 +1,22 @@
+import logging
+import random
+import wishful_upis as upis
+import wishful_agent as wishful_module
+
+__author__ = "Piotr Gawlowicz"
+__copyright__ = "Copyright (c) 2015, Technische Universität Berlin"
+__version__ = "0.1.0"
+__email__ = "{gawlowicz}@tkn.tu-berlin.de"
+
+
+@wishful_module.build_module
+class LinuxNetModule(wishful_module.AgentModule):
+    def __init__(self):
+        super(LinuxNetModule, self).__init__()
+        self.log = logging.getLogger('LinuxNetModule')
+
+    @wishful_module.bind_function(upis.radio.set_channel)
+    def set_channel(self, channel):
+        self.log.debug("Simple Module sets channel: {} on interface: {}".format(channel, self.interface))
+        self.channel = channel
+        return ["SET_CHANNEL_OK", channel, 0]
