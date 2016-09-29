a7ba15036c6bbdef582247c331a826a42f41c0f9
Tolja Zubow <zubow@tkn.tu-berlin.de>
2016-09-29 10:07:32 +0200

    cleanup WiFi module + imports

--- wishful_module_net_linux/linux_net_module.py
+++ wishful_module_net_linux/linux_net_module.py
@@ -19,6 +19,7 @@ import wishful_upis as upis
 from wishful_agent.core import exceptions
 import wishful_agent.core as wishful_module
 
+# TODO:piotr, do we need this???
 #from wishful_framework.upi_arg_classes.flow_id import FlowId
 #from wishful_framework.upi_arg_classes.iptables import SimpleMatch, SimpleTarget, SimplePolicy, SimpleRule, SimpleChain, SimpleTable
 
